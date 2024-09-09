19ffe5d3a840aa9f0769b010637c3fbd07034208
Aditya <149296116+adityaseelam@users.noreply.github.com>
2024-09-09 20:52:09 +0530

    Update Slave2.v

--- Slave2.v
+++ Slave2.v
@@ -1,26 +1,26 @@
-module Slave2(input PCLK,input PRESET,input PENABLE,input PWRITE,input[6:0] PADDR,input[7:0] PWDATA,output reg[7:0] PRDATA,output reg PREADY,output PSLVER,input PSELECT2);
+module Slave2(input PCLK,input PRESET,input PENABLE,input PWRITE,input[6:0] PADDR,input[7:0] PWDATA,output reg[7:0] PRDATA,output reg PREADY,output PSLVERR,input PSELECT2);
   
   reg[7:0] memory2[0:63];
   
-  always@(posedge PCLK)begin
+  always@(*)begin
     
-    if(PRESET) begin
-      PREADY<=0;
+    if(!PRESET) begin
+      PREADY=0;
     end else 
       if(PSELECT2 && !PENABLE && PWRITE) begin
-        PREADY<=0;
+        PREADY=0;
         //memory1[PADDR]<=PWDATA;
       end else 
        if(PSELECT2 && PENABLE && PWRITE) begin
-         PREADY<=1;
-         memory2[PADDR]<=PWDATA;
+         PREADY=1;
+         memory2[PADDR]=PWDATA;
        end else 
          if(PSELECT2 && PENABLE && !PWRITE) begin  
-           PREADY<=1;
-           PRDATA<=memory2[PADDR];
+           PREADY=1;
+           PRDATA=memory2[PADDR];
          end else 
          if(PSELECT2 && !PENABLE && !PWRITE) begin  
-           PREADY<=0;
+           PREADY=0;
          end
   end
     endmodule
